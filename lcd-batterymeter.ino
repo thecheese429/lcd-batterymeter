d74b4b538c7853faaafbc5633e9cb14499fe169d
thecheese429 <thecheese429@hotmail.com>
2016-12-16 18:25:00 -0600

    Tweak resistance check
    Tweak resistance check function and fix the messages displayed on the
    LCD.

--- lcd-batterymeter.ino
+++ lcd-batterymeter.ino
@@ -1,18 +1,19 @@
 #include <LiquidCrystal.h>
 template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; } 
 
-#define INTERVAL 200 //the amount of time to delay between readings
-#define RES 0.62     //define value of resistor used as shunt
-#define ENDVOLT 2.9  //define the voltage at which the test stops
-#define VIN A1       //define the pin number for input A, which will be used to read the cell positive voltage
-#define SHUNT A0     //define the pin number for B, which tells us the voltage on the other side of the shunt resistor
-#define GATE1 0      //the pin used to control the gate on the first mosfet        
-#define GATE2 1       //The pin used to control the gate on the second mosfet
-#define CONTRAST 20   //The contrast level. Change the value between 0 and 255 if you don't see anything
-#define CONTRASTPIN 9 //The pin connected to the contrast on the LCD. Must have a PWM output
-#define RESCHECKVOLT 3.5 //the resistance check will occur as soon as the voltage drops below this value
+#define INTERVAL 200		//the amount of time to delay between readings
+#define SHUNTRES 0.62		//define value of resistor used as shunt
+#define ENDVOLT 2.9			//define the voltage at which the test stops
+#define VIN A1				//define the pin number for input A, which will be used to read the cell positive voltage
+#define SHUNT A0			//define the pin number for B, which tells us the voltage on the other side of the shunt resistor
+#define GATE1 0				//the pin used to control the gate on the first mosfet		   
+#define GATE2 1				//The pin used to control the gate on the second mosfet
+#define CONTRAST 20			//The contrast level. Change the value between 0 and 255 if you don't see anything
+#define CONTRASTPIN 9		//The pin connected to the contrast on the LCD. Pin must have a PWM output
+#define RESCHECKVOLT 3.5	//the resistance check will occur as soon as the voltage drops below this value
+#define RESCHECKDELAY 200	//this is the number of milliseconds delayed between steps in the resistance check
 #define MINSTART 4.10		//This is the minimum voltage on a cell for the test to begin
-#define VOLTREF 5		//Measure the voltage on your 5V reference pin, and put that value here. 
+#define VOLTREF 5			//Measure the voltage on your 5V reference pin, and put that value here. 
 
 
 
@@ -20,18 +21,14 @@ template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg);
 
 double now = 0;
 double lastTime = 0;
-
 double voltage = 0;
 double current = 0;
 double cellResistance = 0;
 double capacity = 0;
 
-boolean zeroVolts = 0;
-boolean newCell = 0;
-
 byte state = 0; //keep track of the current state of the program
 
-byte ohm[8] = {   
+byte ohm[8] = {	  
   0b00000,
   0b00000,
   0b01110,
@@ -47,7 +44,7 @@ byte ohm[8] = {
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 void updateDisplay(){
-  long currTime = now;
+	long currTime = now;
 	int hourTens = currTime/36000000;		//This is a very ugly way to get the digits to print the time.
 	currTime -= hourTens * 36000000;		//don't knock it. It works. Slowly.
 	int hourOnes = currTime / 3600000;
@@ -63,77 +60,93 @@ void updateDisplay(){
 	
 	
 	lcd.clear();
-  lcd.leftToRight();
-  lcd << "V=" << voltage << "v"; //Streaming (<<) is an easier way to write varied outputs like these
-
-
-  lcd.setCursor(15,0);
-  lcd.rightToLeft();
-  lcd << "  " << current;       // this is a cheap way to write something on the right side of the screen
-  lcd.leftToRight();            // Write something right to left (minus one character), then without moving the cursor
-  lcd << "I=" << current << "A";// write the same thing left to right (with all the characters, this time)
-
-  lcd.setCursor(0,1);
-  lcd.leftToRight();
-  lcd << (int)capacity << "mAh";
-
-  lcd.setCursor(8,1);
+	lcd.leftToRight();
+	lcd << "V=" << voltage << "v"; //Streaming (<<) is an easier way to write varied outputs like these
+	lcd.setCursor(15,0);
+	lcd.rightToLeft();
+	lcd << "	" << current;		// this is a cheap way to write something on the right side of the screen
+	lcd.leftToRight();			// Write something right to left (minus one character), which puts the cursor in the righ location,	 
+	lcd << "I=" << current << "A";// then without moving the cursor, write the same thing left to right (with all the characters, this time)
+	lcd.setCursor(0,1);
+	lcd.leftToRight();
+	lcd << (int)capacity << "mAh";	
+	lcd.setCursor(8,1);
 	//lcd << hour(now) << ":" << minute(now) << ":" << second(now) ;
-  lcd << hourTens << hourOnes << ":" << minuteTens << minuteOnes << ":" << secondTens << secondOnes;
+	lcd << hourTens << hourOnes << ":" << minuteTens << minuteOnes << ":" << secondTens << secondOnes;
 }
 
 double capacityCalc(){
-	//take the current values and check the time since last called, then return the charge that has passed since last call in units of mAh
+	//take the measurements and check the time since last called, then return the charge that has passed since last call in units of mAh
 	return(current * (now - lastTime) / 3600 );
 }
 
 void updateMeasurements(){
 	int voltRaw = analogRead(VIN);		//read the values from the pins without doing any math
-	int shuntRaw = analogRead(SHUNT); 	//so that the values are read as close to simultaneously as possible
+	int shuntRaw = analogRead(SHUNT);	//so that the values are read as close to simultaneously as possible
 	
 	lastTime = now;		//shift now back to lastTime
 	now = millis();		//keep track of when the measurements were made
 	
-	voltage = (double)voltRaw *VOLTREF/1024;
-	current = (double)(voltRaw - shuntRaw)*VOLTREF/1024 / RES;
+	voltage = (double)voltRaw *VOLTREF/1024;					//the voltage scales with 8 bits between 0 and the 5v reference pin
+	current = (double)(voltRaw - shuntRaw)*VOLTREF/1024 / SHUNTRES;	// sometimes the 5v pin isn't actually 5 volts, so we use VOLTREF instead of 5
 	
 }
 
 void readResistance(){
-  double v1;
-  double v2;
-  double i1;
-  double i2;
+	double v[3];
+	double i[3];
+	
+	int Vindex = 0;
+	int Iindex = 0;
+	int NumberOfResistanceChecks = 0;
   
 	digitalWrite(GATE1, HIGH);
 	digitalWrite(GATE2, HIGH);
-	delay(10);
+	delay(RESCHECKDELAY);
 	updateMeasurements();
-	v1 = voltage;
-	i1 = current;
-	delay(10);
-	digitalWrite(GATE1, LOW);
-	delay(10);
+	v[Vindex++] = voltage;
+	i[Iindex++] = current;
+	
+	digitalWrite(GATE1, LOW);		
+	delay(RESCHECKDELAY);
 	updateMeasurements();
-	v2 = voltage;
-	i2 = current;
+	v[Vindex++] = voltage;
+	i[Iindex++] = current;
+	
+	digitalWrite(GATE2, LOW);		
+	delay(RESCHECKDELAY);
+	updateMeasurements();
+	v[Vindex++] = voltage;
+	i[Iindex++] = current;
 
+	for(int x = 0; x<3; x++){
+		for(int y = 0; y<x; y++){
+			if((v[x] != v[y]) && (i[x] != i[y])){
+				NumberOfResistanceChecks++;
+				cellResistance += (v[x] - v[y])/(i[y] - i[x]);
+			}
+		}
+	}
+	cellResistance /= NumberOfResistanceChecks;
 	digitalWrite(GATE1, HIGH);
-	cellResistance = (v1 - v2) / (i2-i1); 
+	digitalWrite(GATE2, HIGH);
+	
 }
 
 void setup() {
-  pinMode(GATE1, OUTPUT);       //Set the appropriate pin modes
-  pinMode(GATE2, OUTPUT);
-  pinMode(CONTRASTPIN, OUTPUT);
-  analogWrite(CONTRASTPIN, CONTRAST);//Set the contrast for the LCD screen
-  digitalWrite(GATE1, LOW);
-  digitalWrite(GATE2, LOW);
-  
-  lcd.begin(16, 2);
+	
+	//Serial.begin(9600);
+	pinMode(GATE1, OUTPUT);		//Set the appropriate pin modes
+	pinMode(GATE2, OUTPUT);
+	pinMode(CONTRASTPIN, OUTPUT);
+	analogWrite(CONTRASTPIN, CONTRAST);//Set the contrast for the LCD screen
+	digitalWrite(GATE1, LOW);
+	digitalWrite(GATE2, LOW);
+	
+	lcd.begin(16, 2);
 	lcd.clear();
 	lcd << "hello";
-  lcd.createChar(0, ohm);
+	lcd.createChar(0, ohm);
 	delay(1000);
   
 }
@@ -192,7 +205,7 @@ void loop() {
 		}
 		
 
-	}                    
+	}					 
 	else if(state == 4){ //This is the state in which the capacity is measured until the resistance check
 		digitalWrite(GATE1, HIGH);
 		digitalWrite(GATE2, HIGH);
@@ -205,7 +218,7 @@ void loop() {
 				state = 5;
 			}
 		}
-	}                    
+	}					 
 	else if(state == 5){ //This is the state in which the resistance is measured
 		lcd.clear();
 		lcd << "Testing res";
@@ -214,7 +227,7 @@ void loop() {
 		if(cellResistance != 0){
 			state = 6;
 		}
-	}                    
+	}					 
 	else if(state == 6){ //This is the state in which the capacity is measured, after the resistance check, until the battery gets low
 		digitalWrite(GATE1, HIGH);
 		digitalWrite(GATE2, HIGH);
@@ -229,7 +242,7 @@ void loop() {
 				digitalWrite(GATE2, LOW);				
 			}
 		}
-	}                    
+	}					 
 	else if(state == 7){ //This is the state after the check is complete and the battery is still in place
 		lcd.setCursor(0,0);
 		lcd << "Done.           ";
@@ -246,7 +259,7 @@ void loop() {
 			updateMeasurements();
 			delay(100);
 		}
-	}                    
+	}					 
 	else if(state == 8){ //This is the state after the check is complete and the battery is removed
 		lcd.clear();
 		lcd << "Insert new cell";
@@ -265,7 +278,7 @@ void loop() {
 			updateMeasurements();
 			delay(100);
 		}
-	}                    
+	}					 
 	else if(state == 9){ //This is the state after the measurement is complete and a new battery has been inserted
 		lcd.setCursor(0,0);
 		lcd << "Reset to begin  ";
